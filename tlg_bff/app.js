f3f60ab2ea70b65b60f529d0cb307d08928c21bd
yourlocaldeveloper <aeroyts@icloud.com>
2023-01-27 09:05:09 +0000

    feat(#2): add mongoose and initial setup

--- tlg_bff/app.js
+++ tlg_bff/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const rfidRouter = require('./routes/rfid');
 const app = express();
 const port = 8080;
+
+const main = async () => {
+  await mongoose.connect('mongodb://127.0.0.1:27017/thelocalgame');
+  console.log('DATABASE: Connected');
+};
+
+main().catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
